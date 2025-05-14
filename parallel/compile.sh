f354913a3aa35f0199495149b94cb3bb21af787c
Rosen <rr12520@bristol.ac.uk>
2025-05-14 21:04:47 +0100

    remove flag

--- parallel/compile.sh
+++ parallel/compile.sh
@@ -4,7 +4,7 @@ ml GCC OpenMPI
 
 gfortran ./serial/pi_serial.f90 -o ./serial/pi_serial
 gfortran -fopenmp -o openmp/pi_openmp openmp/pi_openmp.f90
-mpif90 -fno-range-check mpi/pi_mpi.f90 -o mpi/pi_mpi
+mpif90  mpi/pi_mpi.f90 -o mpi/pi_mpi
 mpif90 -fopenmp -o hybrid/pi_hybrid hybrid/pi_hybrid.f90
 gcc -fopenmp -o openmp/hello_openmp openmp/hello_openmp.c
 mpicc -o mpi/hello_mpi mpi/hello_mpi.c
